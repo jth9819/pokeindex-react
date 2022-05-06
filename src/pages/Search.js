d97c4b98ad002f0268f19ba8a85f6ef79f08e193
Justin Harrison <jth9819@gmail.com>
2022-05-06 11:24:34 -0500

    Prepare for input submission

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -23,43 +23,79 @@ function Search() {
     });
   }, [url]);
 
-  // If information is retrieved, store and return.
-  if (pokemon) {
-    let pokemonTypes = null;
-
-    pokemonObject = {
-      name: pokemon.name,
-      image: pokemon.sprites.other.dream_world.front_default,
-      types: pokemon.types,
-    };
-
-    // Check and store types
-    if (pokemon.types.length === 1) {
-      pokemonTypes = pokemonObject.types[0].type.name;
-    }
-    if (pokemon.types.length === 2) {
-      pokemonTypes =
-        pokemonObject.types[0].type.name +
-        " / " +
-        pokemonObject.types[1].type.name;
-    }
-
-    return (
-      <div>
-        <h1>{pokemonObject.name}</h1>;
-        <img src={pokemonObject.image} alt="Pokemon" />
-        <p>{pokemonTypes}</p>
-      </div>
-    );
-  }
-
   return (
-    <div>
-      <h1>IF STATEMENT DIDN'T RUN</h1>
+    <div class="row" id="content-wrap">
+      {/* Title */}
+      <div className="home-header text-center">
+        <h1 className="display-3">
+          <strong>PokéIndex Search</strong>
+        </h1>
+        <small>Enter a Pokemon's Name or National PokeDex Number!</small>
+      </div>
+      {/* Input group */}
+      <div
+        class="input-group rounded col-8 col-sm-8 col-md-8 col-lg-8 mx-auto"
+        id="input-group"
+      >
+        {/* Input field */}
+        <input
+          type="text"
+          /* #box */ class="form-control rounded"
+          id="search-query"
+          placeholder="Search for a Pokemon!"
+          aria-label="Search"
+        />
+        &nbsp;&nbsp;
+        {/* Find button */}
+        <button
+          /* (click)="getSearchedPokemon(box.value); toggleShow()" */ class="input-group-text border-0"
+          id="search-btn"
+        >
+          <i
+            class="fa fa-search"
+            alt="button"
+            value="search"
+            aria-hidden="true"
+            type="button"
+          ></i>
+          &nbsp;Find
+        </button>
+      </div>
     </div>
   );
 
-  // <div>{content}</div>
+  ///////////////////////////////////////////////////////////////////////////////////
+
+  //   if (pokemon) {
+  //     let pokemonTypes = null;
+
+  //     pokemonObject = {
+  //       name: pokemon.name,
+  //       image: pokemon.sprites.other.dream_world.front_default,
+  //       types: pokemon.types,
+  //     };
+
+  //     // Check and store types
+  //     if (pokemon.types.length === 1) {
+  //       pokemonTypes = pokemonObject.types[0].type.name;
+  //     }
+  //     if (pokemon.types.length === 2) {
+  //       pokemonTypes =
+  //         pokemonObject.types[0].type.name +
+  //         " / " +
+  //         pokemonObject.types[1].type.name;
+  //     }
+
+  //     let returnObject = (
+  //       <div>
+  //         <h1>{pokemonObject.name}</h1>;
+  //         <img src={pokemonObject.image} alt="Pokemon" />
+  //         <p>{pokemonTypes}</p>
+  //       </div>
+  //     );
+  //   }
+
+  ///////////////////////////////////////////////////////////////////////////////////
 
   // return (
   //     <div class="row" id="content-wrap">
@@ -81,6 +117,8 @@ function Search() {
   //         </div>
   //     </div>
   // );
+
+  ////////////////////////////////////////////////////////////////////////////////////
 }
 
 export default Search;
