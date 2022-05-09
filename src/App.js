45d2918f1f7b69b6cd667eaad28b45df7d37fd5a
Justin Harrison <jth9819@gmail.com>
2022-05-09 10:44:05 -0500

    Create Browse.js and add title

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "./styles/App.css";
 import AppNavbar from "./components/AppNavbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Search from "./pages/Search"
+import Search from "./pages/Search";
+import Browse from "./pages/Browse";
 import Footer from "./components/Footer";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -12,11 +13,14 @@ function App() {
     <Router>
       <AppNavbar />
       <div className="content">
-        <div className="container"> {/* Applies a set width */}
+        <div className="container">
+          {" "}
+          {/* Applies a set width */}
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/search/" element={<Search />} />
+            <Route path="/browse/" element={<Browse />} />
           </Routes>
         </div>
       </div>
---
+++ src/pages/Browse.js
@@ -0,0 +1,13 @@
+const Browse = () => {
+  return (
+    <div className="home-header text-center">
+      <h1 className="display-3">
+        <strong>PokéIndex Browse</strong>
+      </h1>
+      {/* Subheading */}
+      <small>Browse for all Pokémon in PokéAPI's database!</small>
+    </div>
+  );
+};
+
+export default Browse;
