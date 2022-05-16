d9144549e1f3803e8c3a8111a3c47d6bbf2571c3
Justin Harrison <jth9819@gmail.com>
2022-05-16 15:46:00 -0500

    Add dependency for react placeholder

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import ReactPlaceholder from "react-placeholder";
 import PokemonCardBrowse from "../components/PokemonCardBrowse";
 
 import "../styles/pages/Browse.css";
